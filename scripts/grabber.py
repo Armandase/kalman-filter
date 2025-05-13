9e7897169641f658d5d31884437c79c2a2ad9d39
Dorian PC portable <dorian.boire98@gmail.com>
2025-05-13 16:48:16 +0200

    Feat : Updating the plot to see all data

--- scripts/grabber.py
+++ scripts/grabber.py
@@ -33,12 +33,35 @@ def compute_response(data:dict, client_socket, filter):
     send_msg(client_socket, next_pos)
 
 def display_history(history):
+    fig = plt.figure(figsize=(10, 8))
+
+    ax1 = fig.add_subplot(2, 2, 1, projection='3d')
     true_pos = np.array(history["TRUE POSITION"]).T
-    ax = plt.axes(projection='3d')
-    ax.plot3D(true_pos[0], true_pos[1], true_pos[2], c='red')
-    plt.show()
+    ax1.plot3D(true_pos[0], true_pos[1], true_pos[2], c='red')
+    ax1.set_title("True Position")
+
+    ax2 = fig.add_subplot(2, 2, 2)
     speed = np.array(history['SPEED'])
-    plt.plot(speed)
+    ax2.plot(speed)
+    ax2.set_title("Speed")
+
+    ax3 = fig.add_subplot(2, 2, 3)
+    acceleration = np.array(history['ACCELERATION']).T
+    ax3.plot(acceleration[0], label="X")
+    ax3.plot(acceleration[1], label="Y")
+    ax3.plot(acceleration[2], label="Z")
+    ax3.set_title("Acceleration")
+    ax3.legend()
+
+    ax4 = fig.add_subplot(2, 2, 4)
+    direction = np.array(history['DIRECTION']).T
+    ax4.plot(direction[0], label="X")
+    ax4.plot(direction[1], label="Y")
+    ax4.plot(direction[2], label="Z")
+    ax4.set_title("Direction")
+    ax4.legend()
+
+    plt.tight_layout()
     plt.show()
 
 def get_dict():
