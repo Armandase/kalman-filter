fix: adjust state noise covariance matrix and improve measurement noise covariance initialization

scripts/kalman_filter.py
@@ -6,8 +6,11 @@ class KalmanFilter():
         print(f"Direction : {direction},\nacceleration : {acceleration},\nspeed : {speed},\ntrue_pos: {true_pos}")
         self.A = np.eye(3) # state transition matrix
         self.H = np.eye(3) # used to map the measurement space to the state space
-        self.Q = np.eye(3) # state noise covariance matrix
+        self.Q = np.eye(1) # state noise covariance matrix
         self.R = np.eye(3) # measurement noise covariance matrix
+        for i in range(len(self.R)):
+            self.R[i,i] = pow(10,-(3-i))
+        print(self.R)
 
         self.P = np.eye(3) # error covariance matrix
         self.pred_P = np.eye(3) # prediction of the error covariance matrix