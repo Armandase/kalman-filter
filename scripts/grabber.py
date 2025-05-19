fix: improve KalmanFilter initialization and response computation logic

scripts/grabber.py
@@ -21,27 +21,18 @@ def connect(addr="127.0.0.1", port=4242):
     send_msg(client_socket, msg, addr=complete_addr) # init connexion between imu and client
     return client_socket
 
-# def extract_state(data:dict):
-    # E = np.array([data[""])
 
 def compute_response(data:dict, client_socket, filter):
     if filter is None:
         filter = KalmanFilter(direction=data['DIRECTION'], acceleration=data["ACCELERATION"], speed=data['SPEED'], true_pos=data["TRUE POSITION"])
-        print("estim_x:", filter.estim_x)
-    # response = array_to_reponse(data["TRUE POSITION"])
-    # print("next pos SEND", response)
 
     filter.update_data(direction=data['DIRECTION'], acceleration=data["ACCELERATION"], speed=data['SPEED'], true_pos=data["TRUE POSITION"])
     x_estimated = filter.predict()
     print("\nx_estimated:",x_estimated)
-    velocity = filter.calculate_velocity(data['SPEED'], data['DIRECTION'])
+    velocity = filter.estim_x[3:6]
     filter.update(np.concatenate((velocity, np.array(data["ACCELERATION"]))))
-    next_pos = filter.update_position(velocity=x_estimated[:3], acceleration=x_estimated[3:6])
-    # print("next_pos:", next_pos)
+    next_pos = filter.update_position(velocity=x_estimated[3:6], acceleration=x_estimated[6:9])
     response = array_to_reponse(next_pos)
-    # print("Response: ",response)
-    # print("next pos as str", next_pos)
-    # print('real next pos', data["TRUE POSITION"])
     send_msg(client_socket, response)
     return filter
 
@@ -74,9 +65,9 @@ def read(client_socket, address="127.0.0.1", port=4242):
             if "MSG_END" in data_decode:
                 filter = compute_response(parsed, client_socket, filter)
                 parsed = get_dict()
-                counter += 1
-                if counter > 2:
-                    exit(0)
+                # counter += 1
+                # if counter > 5000:
+                #     exit(0)
             else:
                 for key in parsed.keys():
                     if key in data_decode:

scripts/kalman_filter.py
@@ -36,10 +36,8 @@ class KalmanFilter():
 
         self.speed = speed
         velocity = self.calculate_velocity(speed, direction)
-        # print(velocity)
         self.estim_x = np.concatenate((true_pos, velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
         self.pred_x = np.concatenate((true_pos, velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
-        # print("x : ", self.estim_x)
         self.pos = np.array(true_pos) # position
         self.velocity = None # velocity
 
@@ -88,9 +86,7 @@ class KalmanFilter():
         print("self speed", self.speed)
         print("speed", speed)
         print("direction :", direction)
-        # if speed != None:
-        #     new_velocity = self.calculate_velocity(speed, direction)
-        # else:
+        true_pos = self.pos
         new_velocity = self.calculate_velocity(self.speed, direction)
-        self.estim_x = np.concatenate((true_pos, velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
-        self.pred_x = np.concatenate((true_pos, velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
+        self.estim_x = np.concatenate((true_pos, new_velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
+        self.pred_x = np.concatenate((true_pos, new_velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]