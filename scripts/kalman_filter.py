2dc70b47fd6da74667d8facad6a95846b7eaae71
Dorian PC portable <dorian.boire98@gmail.com>
2025-05-20 15:53:37 +0200

    Feat : Refactoring the code

--- scripts/kalman_filter.py
+++ scripts/kalman_filter.py
@@ -3,90 +3,114 @@ from constants import DELTA_T, ACCEL_NOISE, GYRO_NOISE, GPS_NOISE, VARIANCE_ACCE
 
 class KalmanFilter():
     def __init__(self, direction, acceleration, speed, true_pos):
-        # print(f"Direction : {direction},\nacceleration : {acceleration},\nspeed : {speed},\ntrue_pos: {true_pos}")
-        self.A = np.eye(9) # state transition matrix
-        self.A[0, 3] = DELTA_T  # px depends on vx
-        self.A[1, 4] = DELTA_T  # py depends on vy
-        self.A[2, 5] = DELTA_T  # pz depends on vz
+        print(f"Direction : {direction},\nacceleration : {acceleration},\nspeed : {speed},\ntrue_pos: {true_pos}")
         
-        self.A[3, 6] = DELTA_T  # vx depends on ax
-        self.A[4, 7] = DELTA_T  # vy depends on ay
-        self.A[5, 8] = DELTA_T  # vz depends on az
+        # estimated state vector
+        self.estim_x = np.zeros(6)
+        # Put the velocity in the state vector
+        self.init_state_vector(direction, speed, true_pos)
+        
+        # previous estimated state vector
+        self.prev_estim_x = np.zeros(6)
+        
+        # Kalman gain
+        self.K = None
+        
+         # covariance matrix
+        self.P = np.eye(6) * 0.1
+        
+        # previous covariance matrix
+        self.prev_P = np.eye(6) * 0.1
+        
+        # observation matrix
+        self.H = np.eye(6)
+        
+        # measurement noise covariance matrix
+        self.R = np.eye(6) * 0.1
+        
+        # measurement vector
+        self.Z = np.zeros(6)
+        
+        # identity matrix
+        self.I = np.eye(6)
+        
+        #state transition matrix
+        self.A = np.eye(6)
+        self.A[0:3, 3:6] = np.eye(3) * DELTA_T
 
-        self.H = np.zeros((6, 9)) # used to map the measurement space to the state space
-        for i in range(6):
-            self.H[i, i + 3] = 1
-        self.Q = np.diag([       # Process noise covariance Q (9x9)
-            GPS_NOISE, GPS_NOISE, GPS_NOISE,       # position x, y, z
-            GYRO_NOISE, GYRO_NOISE, GYRO_NOISE,    # velocity x, y, z
-            ACCEL_NOISE, ACCEL_NOISE, ACCEL_NOISE  # acceleration x, y, z
+        # Control input vector
+        self.U = np.array(acceleration)
+        
+        # COntrol input matrix
+        self.B = np.array([
+            [0.5 * DELTA_T ** 2, 0, 0],
+            [0, 0.5 * DELTA_T ** 2, 0],
+            [0, 0, 0.5 * DELTA_T ** 2],
+            [DELTA_T, 0, 0],
+            [0, DELTA_T, 0],
+            [0, 0, DELTA_T]
         ])
-        # self.R = np.diag([
-        #     0.01,        # speed variance (σ=0.1²)
-        #     1e-6, 1e-6, 1e-6,   # accelerometer noise (σ=0.001²)
-        #     1e-4, 1e-4, 1e-4    # gyroscope/direction noise (σ=0.01²)
-        # ])
-        self.R = np.diag([
-            VARIANCE_ACCEL, VARIANCE_ACCEL, VARIANCE_ACCEL,   # accelerometer noise (σ=0.001²)
-            VARIANCE_GYRO, VARIANCE_GYRO, VARIANCE_GYRO    # gyroscope/direction noise (σ=0.01²)
+
+        # Process noise covariance matrix
+        sigma_pos = 0.5 * (DELTA_T ** 2) * ACCEL_NOISE
+        sigma_vel = DELTA_T * ACCEL_NOISE
+        
+        # Calculating variance for position and velocity
+        var_pos = sigma_pos ** 2
+        var_vel = sigma_vel ** 2
+
+        # Process noise covariance matrix
+        self.Q = np.diag([
+            var_pos[0], var_pos[1], var_pos[2],
+            var_vel[0], var_vel[1], var_vel[2]
         ])
 
-        self.P = np.eye(9) # error covariance matrix
-        self.pred_P = np.eye(3) # prediction of the error covariance matrix
 
-        self.speed = speed
-        velocity = self.calculate_velocity(speed, direction)
-        self.estim_x = np.concatenate((true_pos, velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
-        self.pred_x = np.concatenate((true_pos, velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
-        self.pos = np.array(true_pos) # position
-        self.velocity = None # velocity
-
-    def predict(self):
-        self.pred_x = self.A @ self.estim_x
-        self.pred_P = self.A @ self.P @ self.A.T + self.Q
-        return self.pred_x
-
-    def update(self, z):
+    def measurement_update(self, direction, acceleration, speed, true_pos):
         """
-        Update the state with the measurement z.
-        z is the measurement vector.
-        """
-        K = self.pred_P @ self.H.T @ np.linalg.inv(self.H @ self.pred_P @ self.H.T + self.R)
-
-        self.estim_x = self.pred_x + K @ (z - self.H @ self.pred_x)
-        self.P = self.pred_P - K @ self.H @ self.pred_P
-
-    def update_position(self, velocity, acceleration, delta_t=DELTA_T):
-        # self.velocity = [speed * d for d in direction]
-        self.velocity = np.array(velocity)
-        acceleration = np.array(acceleration)
-        self.pos = self.pos + self.velocity * delta_t + 0.5 * acceleration * (np.array(delta_t)**2)
-        return self.pos
-
-
-    def calculate_velocity(self, speed, direction):
-        """
-        Calculate the velocity vector given speed and direction.
-
-        Parameters:
-        - speed: A scalar value representing the speed.
-        - direction: A NumPy array representing the direction.
-
-        Returns:
-        - velocity: A vector representing the velocity.
+        Update the measurement vector with the current acceleration and direction
+        Here it is each 3 seconds when we receive a new GPS position
         """
 
-        velocity = speed * np.array(direction)
-        return velocity
+        # Update the measurement vector
+        self.Z[0:3] = np.array(true_pos)
+        self.Z[3:6] = self.calculate_velocity(speed, direction)
+
+        # Measurement update
+        self.K = self.prev_P @ self.H.T @ np.linalg.inv(self.H @ self.prev_P @ self.H.T + self.R)
+        
+        # Update the state estimate
+        self.prev_estim_x = self.estim_x.copy()
+        self.estim_x = self.prev_estim_x + self.K @ (self.Z - self.H @ self.prev_estim_x)
+        
+        # Update the covariance matrix
+        self.prev_P = self.P.copy()
+        self.P = (self.I - self.K @ self.H) @ self.prev_P
+
+
+    def time_update(self):
+        """ Update the state vector and covariance matrix based on the model"""
+        
+        # Project the state ahead
+        self.estim_x = self.A @ self.prev_estim_x + self.B @ self.U
+        
+        # Project the error covariance ahead
+        self.P = self.A @ self.prev_P @ self.A.T + self.Q
     
-    def update_data(self, direction, acceleration, speed, true_pos):
-        """
-        Update the filter with the new data sent
-        """
-        print("self speed", self.speed)
-        print("speed", speed)
-        print("direction :", direction)
-        true_pos = self.pos
-        new_velocity = self.calculate_velocity(self.speed, direction)
-        self.estim_x = np.concatenate((true_pos, new_velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
-        self.pred_x = np.concatenate((true_pos, new_velocity, acceleration)) # [px, py, pz, vx, vy, vz, accelx, accely, accelz]
+    
+    def init_state_vector(self, direction, speed, true_pos):
+        """ Initialize the state vector with the given true position and velocity"""
+        
+        self.estim_x[0:3] = np.array(true_pos)
+        self.estim_x[3:6] = self.calculate_velocity(speed, direction)
+    
+    
+    def calculate_velocity(self, speed, direction):
+        """ Calculate the velocity based on the current state vector """
+        
+        direction = np.array(direction)
+        norm = np.linalg.norm(direction)
+        if norm != 0:
+            direction = direction / norm
+        velocity = speed * direction
+        return velocity
